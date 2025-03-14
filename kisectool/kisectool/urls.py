332620ceb2a180b2ea94c98357488537247b2d23
xiaqiqi <1561251851@qq.com>
2025-03-14 14:23:18 +0800

    重构

--- kisectool/kisectool/urls.py
+++ kisectool/kisectool/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from portscan import views as portscan_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('assets/', portscan_views.asset_list, name='asset_list'),
 ]
