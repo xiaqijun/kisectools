5905a46952eb0d42aef34c19fd1752942b797c0e
xiaqiqi <1561251851@qq.com>
2025-04-08 15:48:38 +0800

    已完成zoomeye插件增删改查功能

--- plugins/zoomeye.py
+++ plugins/zoomeye.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import urllib3
+import uuid
 # 忽略 InsecureRequestWarning 警告
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 class ZoomEyePlugin:
@@ -50,11 +51,71 @@ class ZoomEyePlugin:
         response=requests.get(url=url,headers=headers,verify=False)
         if response.status_code!=200:
             return response.text
-    
         return response.json()['data']['status']
-if __name__ == '__main__':
-    zoomeye=ZoomEyePlugin(username='secmg', password='zoomeye25@802q1',zoomeye_ip='172.17.11.27')
-    token=zoomeye.get_token()
-    #task_id=zoomeye.create_task('172.17.11.34','test_task2','22,80')
-    status=zoomeye.get_task_status("67f3802042c076a9ca6532ed")
-    print(status)
+    
+    def get_task_result(self,task_id):
+        url=f"https://{self.zoomeye_ip}/api/v4/external/detection?taskId={task_id}"
+        headers={
+            'b-json-web-token':self.get_token()
+        }
+        response=requests.get(url=url,headers=headers,verify=False)
+        count=response.json()['data']['count']
+        page=response.json()['data']['page']
+        page_size=response.json()['data']['pageSize']
+        id=uuid.uuid4()
+        result_file=f"tmp/result_{id}.txt"
+    
+        result_list=self.result(response)
+        for page in range(2,int(count/page_size)+2):
+            url=f"https://{self.zoomeye_ip}/api/v4/external/detection?taskId={task_id}&page={page}"
+            response=requests.get(url=url,headers=headers,verify=False)
+            result_list=self.result(response)
+            self.write_result(result_list,result_file)
+        return result_file
+    
+    def write_result(self,result_list,result_file):
+        with open(result_file,'a') as f:
+            for ip_port in result_list:
+                f.write(json.dumps(ip_port) + '\n')
+
+    def result(self,response):
+        ip_port_list=[]
+        for ip_port in response.json()['data']['list']:
+            ip=ip_port['ip']
+            for ports in ip_port['ports']:
+                port=ports['port']
+                service=ports['service']
+                ip_port_list.append(
+                    {
+                        'host':ip,
+                        'port':port,
+                        'status':'opened',
+                        'service':service
+                    }
+                )
+        return ip_port_list
+    def delete_task(self,task_id):
+        url=f"https://{self.zoomeye_ip}/api/v4/external/deleteTask"
+        headers={
+            'b-json-web-token':self.get_token()
+        }
+        data={
+            "taskId":task_id
+        }
+        response=requests.post(url=url,headers=headers,json=data,verify=False)
+        if response.status_code!=200:
+            return False
+        return True
+    
+    def stop_task(self,task_id):
+        url=f"https://{self.zoomeye_ip}/api/v4/external/stopTask"
+        headers={
+            'b-json-web-token':self.get_token()
+        }
+        data={
+            "taskId":task_id
+        }
+        response=requests.post(url=url,headers=headers,json=data,verify=False)
+        if response.status_code!=200:
+            return False
+        return True
